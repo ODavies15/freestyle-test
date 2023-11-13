5614c4a1c02219c47ab5ae3e15aefc419a7cb50f
ODavies15 <oliver.davies15@hotmail.co.uk>
2023-11-13 14:48:36 +0000

    Archiving steps 2

--- run.sh
+++ run.sh
@@ -2,3 +2,9 @@ echo "Testing"
 echo "Check auto trigger"
 echo "Take 2"
 echo "IP address fixed"
+
+echo "Hello from the Jenkins job named: ${JOB_NAME}"
+ 
+touch 1.txt 2.txt 3.txt 4.txt 5.txt
+ 
+zip archive.zip *.txt
